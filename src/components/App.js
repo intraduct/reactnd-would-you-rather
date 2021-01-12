f65cccd7bedecdb073f4a5acaeb39aab78bb3316
Sebastian Reuter <sebastian.reuter@live.de>
2021-01-12 09:57:05 +0100

    refactor: removed unused import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import LoadingBar from 'react-redux-loading-bar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
